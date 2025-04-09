58291df3ba2dd52adca910b4a3081875b8813aa8
Yochay Cohen-Or <yochai.cohenor@gmail.com>
2025-04-09 17:11:37 +0300

    Design some more

--- client/src/components/ChatController/ChatController.tsx
+++ client/src/components/ChatController/ChatController.tsx
@@ -1,9 +1,11 @@
 import React, { useState } from "react"
-import { IoReturnUpBackOutline } from "react-icons/io5"
+import { IoReturnUpBackOutline as ReturnIcon } from "react-icons/io5"
 import { DotLoader } from "react-spinners"
-import { IconButton, Input, Textarea } from "@mui/joy"
+import { IconButton, Textarea } from "@mui/joy"
 import { useChat } from '@ai-sdk/react'
 
+import ChatMessage from "../ChatMessage/ChatMessage"
+import UserInput from "../UserInput/UserInput";
 import { checkForExistingEvals } from "../../api/backend/evaluations"
 import { UserAction } from "../../types/users"
 
@@ -14,31 +16,58 @@ export interface ChatControllerProps {
   selectAction: (action: UserAction | null) => void
 }
 
-const INITIAL_MESSAGE = {
-  id: '1',
+const INITIAL_MESSAGES = [{
+  id: 'zero-message',
+  role: 'user' as const,
+  content: 'יש לי מספר החלטה',
+}, {
+  id: 'request-decison-number',
   role: 'assistant' as const,
-  content: 'הזן מספר החלטה',
-}
+  content: 'מעולה, לניתוח החלטה יש להזין את המספר בתיבה',
+}]
 const THIRD_MESSAGE = {
-  id: '3',
+  id: 'request-decision-text',
   role: 'assistant' as const,
   content: 'הזן את תוכן ההחלטה',
+}
+
+const INPUT_PROPS_BY_STATE = {
+  requestDecisionNumber: {
+    placeholder: "מה מספר ההחלטה?",
+    type: 'text',
+  },
+  requestDecisionText: {
+    placeholder: "הדביקו כאן את תוכן ההחלטה",
+    type: 'textarea',
+  },
 }
 
 function ChatController({ selectAction }: ChatControllerProps) {
   const [decisionNumber, setDecisionNumber] = useState<number>(0)
   const { messages, input, setInput, setMessages, handleSubmit, status, stop, error, reload } = useChat({
     api: '/api/evaluations/',
-    initialMessages: [INITIAL_MESSAGE],
+    initialMessages: INITIAL_MESSAGES,
   });
 
   const lastMessage = messages[messages.length - 1]
-  const isLastMessageRoleUser = lastMessage.role === 'user'
-
+  const isRequestDecisionNumberState = (
+    lastMessage.role === 'assistant' && 
+    lastMessage.id === 'request-decison-number'
+  )
+  // TODO: State Machine
   const isLastMessageFinal = (
     (messages[messages.length - 1]?.annotations?.[0] as { finalMessage?: boolean })?.finalMessage ||
-    messages.length > 3
+    messages.length > 4
   )
+  const isRequestDecisionTextState = (
+    lastMessage.role === 'assistant' &&
+    lastMessage.id === 'request-decison-text'
+  )
+
+  // TODO: State Machine
+  const currInputProps = isRequestDecisionNumberState
+    ? INPUT_PROPS_BY_STATE.requestDecisionNumber
+    : isRequestDecisionTextState ? INPUT_PROPS_BY_STATE.requestDecisionText : null
 
   const handleInputChange = (event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
     setInput(event.target.value)
@@ -50,10 +79,10 @@ function ChatController({ selectAction }: ChatControllerProps) {
       return
     }
 
-    if (messages.length === 1) {
+    if (isRequestDecisionNumberState) {
       setMessages((currMessages) => {
         const newMessages = [...currMessages, {
-          id: '2',
+          id: 'decision-number',
           role: 'user' as const,
           content: `מספר החלטה: ${input}`,
         }]
@@ -61,19 +90,21 @@ function ChatController({ selectAction }: ChatControllerProps) {
       })
       
       const newDecisionNumber = parseInt(input)
-      const [err, evaluation] = await checkForExistingEvals(newDecisionNumber)
       setInput('')
       setDecisionNumber(newDecisionNumber)
+      
+      const [err, evaluation] = await checkForExistingEvals(newDecisionNumber)
 
       if (err) {
         console.error(err)
         return
       }
+
       if (evaluation) {
         setMessages((currMessages) => [...currMessages, {
-          id: '3',
+          id: 'evaluation',
           role: 'assistant',
-          content: `תוכן ההחלטה: ${evaluation}`,
+          content: `מצאתי את ההחלטה שחיפשת, הנה הניתוח שביצעתי עבורה:\n${evaluation}`,
           annotations: [{ finalMessage: true }],
         }])
       } else {
@@ -81,7 +112,7 @@ function ChatController({ selectAction }: ChatControllerProps) {
       }
     }
     
-    if (messages.length === 3) {
+    if (messages.length === 4 && !isLastMessageFinal) {
       handleSubmit(event, {
         body: { decisionNumber },
       })
@@ -91,36 +122,24 @@ function ChatController({ selectAction }: ChatControllerProps) {
   }
 
   return (
-    <>
+    <div className='chat-interface'>
       <div className='return-to-actions'>
         <IconButton
           className='return-button'
           onClick={() => selectAction(null)}
           variant="soft"
+          title='חזרה למסך הראשי'
         >
-          <IoReturnUpBackOutline />
+          <ReturnIcon />
         </IconButton>
       </div>
 
-      {messages.map(message => (
-        <div key={message.id}>
-          {message.role === 'user' ? 'User: ' : 'AI: '}
-          {message.content}
-        </div>
-      ))}
+      <div className="messages-section">
+        {messages.map((message) => <ChatMessage key={message.id} message={message} />)}
+      </div>
 
       <form className="inputs-form" onSubmit={processUserAction}>
-        {messages.length === 1 && (
-          <>
-            <Input
-              className='decision-number-input'
-              placeholder="מה מספר ההחלטה?"
-              value={input}
-              onChange={handleInputChange}
-            />
-          </>
-        )}
-        {messages.length === 3 && !isLastMessageFinal && (
+        {isRequestDecisionTextState && (
           <>
             <Textarea
               className='decision-text-input'
@@ -131,28 +150,37 @@ function ChatController({ selectAction }: ChatControllerProps) {
             />
           </>
         )}
-        {!isLastMessageFinal && !isLastMessageRoleUser && (
-          <button className='evaluate-button' type='submit'>Next</button>
+
+        {(status === 'submitted' || status === 'streaming') && (
+          <div>
+            {status === 'submitted' && <div className='loader-container'>
+              <DotLoader className="loader" />
+            </div>}
+            <button type="button" onClick={() => stop()}>
+              Stop
+            </button>
+          </div>
+        )}
+        {error && (
+          <>
+            <div>An error occurred.</div>
+            <button type="button" onClick={() => reload()}>
+              Retry
+            </button>
+          </>
+        )}
+        {!isLastMessageFinal && lastMessage.role === 'assistant' && (
+          <div className='user-input-container'>
+            <UserInput
+              className='user-input'
+              value={input}
+              onChange={handleInputChange}
+              {...currInputProps}
+            />
+          </div>
         )}
       </form>
-
-      {(status === 'submitted' || status === 'streaming') && (
-        <div>
-          {status === 'submitted' && <DotLoader className="loader" />}
-          <button type="button" onClick={() => stop()}>
-            Stop
-          </button>
-        </div>
-      )}
-      {error && (
-        <>
-          <div>An error occurred.</div>
-          <button type="button" onClick={() => reload()}>
-            Retry
-          </button>
-        </>
-      )}
-    </>
+    </div>
   )
 }
 
---
+++ client/src/components/ChatMessage/ChatMessage.tsx
@@ -0,0 +1,33 @@
+import { Message } from "@ai-sdk/react";
+import { Avatar } from "@mui/joy";
+import Markdown from "react-markdown";
+import remarkGfm from "remark-gfm";
+import { FaRegUserCircle as UserIcon } from "react-icons/fa";
+
+import "./ChatMessage.css";
+
+const ROLE_TO_IMG = {
+    assistant: '/CECI-logo.png',
+}
+
+export interface ChatMessageProps {
+    message: Message;
+}
+
+function ChatMessage ({ message }: ChatMessageProps) {
+    return (
+        <div key={message.id} className="message">
+            <div className='message-role'>
+                {message.role === 'assistant' 
+                    ? <Avatar src={ROLE_TO_IMG[message.role]} size="sm" /> 
+                    : <Avatar size='sm'><UserIcon size='sm' /></Avatar>
+                }
+            </div>
+            <div className="message-text">
+                <Markdown remarkPlugins={[remarkGfm]}>{message.content}</Markdown>
+            </div>
+        </div>
+    )
+}
+
+export default ChatMessage
